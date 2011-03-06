dbdaf24e1b08313a1a1b27c891d7cafd7106b076
Basil Shkara <basil@neat.io>
2011-03-06 15:05:10 +1100

    Initial Commit

---
+++ Objective NodeTests/Objective_NodeTests.h
@@ -0,0 +1,17 @@
+//
+//  Objective_NodeTests.h
+//  Objective NodeTests
+//
+//  Created by Basil Shkara on 6/03/11.
+//  Copyright 2011 Neat IO Pty Ltd. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface Objective_NodeTests : SenTestCase {
+@private
+    
+}
+
+@end
